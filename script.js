3b7300be1507fe5dddf28da058291426b7db1616
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-21 21:12:49 +0530

    complete adding voice

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
 
-    voices.forEach((voice, i) => (voiceSelect.options[i]) = new )
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)))
 }
 document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
